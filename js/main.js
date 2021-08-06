06a10c4433be422ca8a9f10dea13da795daa266d
Carla Lafaurie <clafaurie@ufl.edu>
2021-08-05 20:59:57 -0400

    added smooth scroll java

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
+$( () => {
 
-$.backstretch([
-  "images/beach.jpg",
-  "images/beach2.jpg",
-  "images/beach3.jpg",
-  "images/beach4.jpg"
-],
+	//On Scroll Functionality
+	$(window).scroll( () => {
+		var windowTop = $(window).scrollTop();
+		windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
+		windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
+	});
 
-  {duration: 4000, transition: "fade", transitionDuration: 1000, animateFirst: false}
+	//Click Logo To Scroll To Top
+	$('#logo').on('click', () => {
+		$('html,body').animate({
+			scrollTop: 0
+		},500);
+	});
 
-);
+	//Smooth Scrolling Using Navigation Menu
+	$('a[href*="#"]').on('click', function(e){
+		$('html,body').animate({
+			scrollTop: $($(this).attr('href')).offset().top - 100
+		},500);
+		e.preventDefault();
+	});
 
-$("#container").backstretch("images/blackbackground.jpg");
+	//Toggle Menu
+	$('#menu-toggle').on('click', () => {
+		$('#menu-toggle').toggleClass('closeMenu');
+		$('ul').toggleClass('showMenu');
 
-alert("Hello World!");
+		$('li').on('click', () => {
+			$('ul').removeClass('showMenu');
+			$('#menu-toggle').removeClass('closeMenu');
+		});
+	});
+
+});
